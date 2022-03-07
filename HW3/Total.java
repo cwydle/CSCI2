3009085e563823c44e00743b0ea59c28ce199733
cwydle <90412176+cwydle@users.noreply.github.com>
2022-03-06 22:03:10 -0500

    Update Total.java

--- HW3/Total.java
+++ HW3/Total.java
@@ -31,6 +31,10 @@ public class Total
          String B = in.next();
          int C = in.nextInt();
          totalB += C; 
+         if (totalB + totalG > tenmillionth && found == false) {
+            All = B; 
+            found = true; 
+         }
          double D = in.nextDouble();
          String E = in.next();         
          int F = in.nextInt();
